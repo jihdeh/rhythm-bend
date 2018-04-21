20e8158d5e581fd1c7b947eb57d00cabc558f7ce
john Anisere <johnanisere@gmail.com>
2018-04-21 11:44:42 +0100

    feat(smsNotification)-added sms notification to user seed

--- server/db/seeds/user.js
+++ server/db/seeds/user.js
@@ -1,9 +1,10 @@
 import User from "../../../app/user/userModel";
 import log from "../../../util/log";
 import _ from "lodash";
-import generate from '../../../util/generatecode'
+import generate from "../../../util/generatecode";
+import sendSms from "../../../util/sms-client";
 
-export default function () {
+export default function() {
   log.info("Seeding the Database");
 
   const users = [
@@ -14,14 +15,16 @@ export default function () {
       password: "test",
       type: "voter",
       phoneNumber: 2348144194590
-    }, {
+    },
+    {
       email: "jimmy@x.com",
       firstName: "kim",
       lastName: "kar",
       password: "test",
       type: "contestant",
       phoneNumber: 2348144194590
-    }, {
+    },
+    {
       email: "xoko@x.com",
       firstName: "jake",
       lastName: "mark",
@@ -31,9 +34,10 @@ export default function () {
     }
   ];
 
-  const createDoc = (model, doc) => new Promise((resolve, reject) => new model(doc).save((err, saved) => (err
-    ? reject(err)
-    : resolve(saved))));
+  const createDoc = (model, doc) =>
+    new Promise((resolve, reject) =>
+      new model(doc).save((err, saved) => (err ? reject(err) : resolve(saved)))
+    );
 
   const cleanDB = () => {
     log.info("...cleaning the DB");
@@ -44,21 +48,25 @@ export default function () {
   };
 
   const createUsers = async data => {
-    const allusers = await User.find({type: "contestant"});
-    let promises = users.map(async(user, key) => {
+    const allusers = await User.find({ type: "contestant" });
+    let promises = users.map(async (user, key) => {
       let newuser = new User(user);
       const uniqueCode = await generate(newuser.firstName, User);
+      await sendSms("+" + newuser.phoneNumber);
       newuser.password = newuser.hashPassword(user.password, newuser.saltPassword());
-      if (user.type === "contestant") 
-        newuser.uniqueCode = uniqueCode;
+      if (user.type === "contestant") newuser.uniqueCode = uniqueCode;
       return newuser.save();
     });
 
-    return Promise
-      .all(promises)
-      .then(users => _.merge({
-        users: users
-      }, data || {}))
+    return Promise.all(promises)
+      .then(users =>
+        _.merge(
+          {
+            users: users
+          },
+          data || {}
+        )
+      )
       .catch(err => console.log("user exists"));
   };
 
