05b2bc5740963d44e9e37da2ce9229e08e098e81
jihdeh <jide.b.tade@gmail.com>
2018-04-26 18:20:25 +0100

    Add cors package for access origin control

--- app/transaction/transactionController.js
+++ app/transaction/transactionController.js
@@ -9,26 +9,25 @@ const reject = (e, ctx) => {
   };
 };
 
-const resolve = (_res,ctx)=>{
-    ctx.status = 200;
-    ctx.body = {
-      data: {
-        status: _res.data.data.status,
-        message: _res.data.message,
-        gateway_response: _res.data.data.gateway_response,
-        authorization: _res.data.data.authorization,
-        reference: _res.data.data.reference,
-        log: _res.data.data.log
-      }
-    };
+const resolve = (_res, ctx) => {
+  ctx.status = 200;
+  ctx.body = {
+    data: {
+      status: _res.data.data.status,
+      message: _res.data.message,
+      gateway_response: _res.data.data.gateway_response,
+      authorization: _res.data.data.authorization,
+      reference: _res.data.data.reference,
+      log: _res.data.data.log
+    }
+  };
 };
 
 exports.verify = async ctx => {
   const reference = ctx.params.reference;
-
   try {
-    const _res = await axios.get(config.endpoint + reference, { headers: config.headers });
-    resolve(_res,ctx);
+    const _res = await axios.get(`${config.endpoint}${reference}`, { headers: config.headers });
+    resolve(_res, ctx);
   } catch (e) {
     reject(e, ctx);
   }
--- server/app.js
+++ server/app.js
@@ -1,36 +1,38 @@
 import koa from "koa";
+import cors from "@koa/cors";
 import forward from "koa-forward-request";
 import mount from "koa-mount";
 import log from "../util/log";
 import Api from "./api";
-import connection from "./db/connection"
+import connection from "./db/connection";
 import Frontend from "./frontend";
-import seed from './db/seeds/user'
-import session from 'koa-session'
-import passport from 'koa-passport'
-import auth from './auth'
+import seed from "./db/seeds/user";
+import session from "koa-session";
+import passport from "koa-passport";
+import auth from "./auth";
 
 function App() {
   const app = new koa();
+  app.use(cors());
   //sessions
-  app.keys = ['super-secret-key']
-  app.use(session(app))
+  app.keys = ["super-secret-key"];
+  app.use(session(app));
 
   //authentication
-  auth()
-  app.use(passport.initialize())
-  app.use(passport.session())
+  auth();
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   //mongoose connection
-  connection(app)
+  connection(app);
 
   //seed db
-  seed()
+  seed();
 
   forward(app);
-  app.use(mount("/", Frontend())).use(mount("/api", Api()))
-    //you will now make api calls with "/api" as default base
-    //for example GET => /api/getNotes, /api/postInfo
+  app.use(mount("/", Frontend())).use(mount("/api", Api()));
+  //you will now make api calls with "/api" as default base
+  //for example GET => /api/getNotes, /api/postInfo
 
   return app;
 }
