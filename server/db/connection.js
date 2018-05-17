54e367e32c513a93937149d771bb71110fe7ee65
jihdeh <jide.b.tade@gmail.com>
2018-05-17 22:48:06 +0100

    Add options to keep db connection alive

--- server/db/connection.js
+++ server/db/connection.js
@@ -2,9 +2,12 @@ import mongoose from "mongoose";
 
 export default function(app) {
   const url = process.env.DB_URL;
-
+  const options = {
+    keepAlive: 300000,
+    connectTimeoutMS: 30000
+  };
   mongoose
-    .connect(url)
+    .connect(url, options)
     .then(() => console.info(`==> ✅ Database connection established`))
     .catch(err => console.error(err));
 }
