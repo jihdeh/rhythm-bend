dc9446d16e6c327469f85dc8cf0cf40d1427e152
John <john@busha.co>
2018-05-29 15:47:25 +0100

    updated voting system

--- app/edit-routes.js
+++ app/edit-routes.js
@@ -42,31 +42,38 @@ const donations = async ctx => {
 };
 
 const votings = async ctx => {
-  try {
-    // /api/vote/841736995?username=jakeRJWL&voteCount=2 url sample.
-    const username = ctx.query.username;
-    const voteCount = ctx.query.voteCount;
-    const findContestant = await User.findOne({ username }).select("numberOfVotesAttained");
+  const status = await Status.find();
+  const open = await status[0].votingOpen;
+  if (open) {
+    try {
+      // /api/vote/841736995?username=jakeRJWL&voteCount=2 url sample.
+      const username = ctx.query.username;
+      const voteCount = ctx.query.voteCount;
+      const findContestant = await User.findOne({ username }).select("numberOfVotesAttained");
 
-    const incVote = +findContestant.numberOfVotesAttained + +voteCount;
+      const incVote = +findContestant.numberOfVotesAttained + +voteCount;
 
-    const isVerified = await verifyPaystackResponse(ctx);
+      const isVerified = await verifyPaystackResponse(ctx);
 
-    if (!isVerified) {
-      ctx.status = 404;
-      ctx.body = "Payment not valid";
-      return;
+      if (!isVerified) {
+        ctx.status = 404;
+        ctx.body = "Payment not valid";
+        return;
+      }
+      const updateContestantVote = await User.findOneAndUpdate(
+        { username },
+        { numberOfVotesAttained: incVote },
+        { new: true }
+      ).select("numberOfVotesAttained email username");
+      ctx.status = 200;
+      ctx.body = updateContestantVote;
+    } catch (e) {
+      ctx.status = 400;
+      ctx.response = { message: "Error placing vote" };
     }
-    const updateContestantVote = await User.findOneAndUpdate(
-      { username },
-      { numberOfVotesAttained: incVote },
-      { new: true }
-    ).select("numberOfVotesAttained email username");
-    ctx.status = 200;
-    ctx.body = updateContestantVote;
-  } catch (e) {
+  } else {
     ctx.status = 400;
-    ctx.response = { message: "Error placing vote" };
+    ctx.body = { message: "voting is currently closed" };
   }
 };
 
@@ -93,7 +100,12 @@ const uploadProfileImage = async ctx => {
   // content-type: multipart/from
   // username, image as file
   try {
-    const { fields: { username }, files: { image: { path } } } = ctx.request.body;
+    const {
+      fields: { username },
+      files: {
+        image: { path }
+      }
+    } = ctx.request.body;
     const upload = await Cloudinary.v2.uploader.upload(path, {
       public_id: username,
       faces: true
